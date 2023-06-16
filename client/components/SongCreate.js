0212f9289fdcd3d973e8318fc0ada290f8300c24
Chris Wren <chris.wren@cef.co.uk>
2023-06-16 15:19:59 +0100

    Handle query and redirect after mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
+import { Link, hashHistory } from "react-router";
+import query from "../queries/fetchSongs";
 
 const SongCreate = (props) => {
   const [title, setTitle] = useState("");
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.mutate({
-      variables: {
-        title,
-      },
-    });
+    props
+      .mutate({
+        variables: {
+          title,
+        },
+        refetchQueries: [{ query }],
+      })
+      .then(() => hashHistory.push("/"));
   };
 
   return (
     <div>
+      <Link to="/">Back</Link>
       <h3>Create a New Song</h3>
       <form onSubmit={submitHandler}>
         <label>Song Title:</label>
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import query from "../queries/fetchSongs";
 
 const SongList = (props) => {
+  const onSongDelete = (id) => {
+    props.mutate({ variables: { id } }).then(() => props.data.refetch());
+  };
+
   const renderSongs = () => {
     return props.data.songs.map((song) => {
       return (
         <li key={song.id} className="collection-item">
           {song.title}
+          <i className="material-icons" onClick={() => onSongDelete(song.id)}>
+            delete
+          </i>
         </li>
       );
     });
@@ -18,19 +27,21 @@ const SongList = (props) => {
   }
 
   return (
-    <ul className="collection">
-      <renderSongs />
-    </ul>
+    <div>
+      <ul className="collection">{renderSongs()}</ul>
+      <Link to="/songs/new" className="btn-floating btn-large red right">
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
   );
 };
 
-const query = gql`
-  {
-    songs {
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
       id
-      title
     }
   }
 `;
 
-export default graphql(query)(SongList);
+export default graphql(mutation)(graphql(query)(SongList));
